172b617b7bbfe66b5351dd8b15b5e36b842dc260
xxq <hello@xxq.cn>
2024-10-07 20:37:59 +0800

    test

--- src/main/java/com/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.helloworld.controller;
 
 import com.helloworld.entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @PostMapping("/login")
-    public String postMethod(@RequestBody User user) {
+    @GetMapping("/login")
+    public String login(User user) {
+
+        System.out.println(user.getUserName() + " " + user.getPassword());
         if ( !(user.getUserName() != null && user.getUserName().equals("test"))) {
             return "error";
         }
