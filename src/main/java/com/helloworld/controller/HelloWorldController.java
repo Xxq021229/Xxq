1ad1a2b10d9e7a3d2278a5a0b943af85a3cd79a3
xxq <hello@xxq.cn>
2024-10-07 20:40:26 +0800

    test

--- src/main/java/com/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -2,8 +2,6 @@ package com.helloworld.controller;
 
 import com.helloworld.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
